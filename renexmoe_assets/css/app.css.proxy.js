a7e7be50277af0051f6d456bb8e1e3db101f4952
186526 <i@186526.xyz>
2021-01-15 22:16:37 +0800

    new Version

---
+++ renexmoe_assets/css/app.css.proxy.js
@@ -0,0 +1,10 @@
+// [snowpack] add styles to the page (skip if no document exists)
+if (typeof document !== 'undefined') {
+  const code = "@font-face {\n  font-family: \"Product Sans\";\n  src: local(\"Product Sans Regular\"), local(\"Google Sans Regular\"),\n    url(https://cdn.jsdelivr.net/gh/186526/jsdelivr@1.1.8/static/ProductSans-Regular.woff2)\n      format(\"woff2\");\n}\n\nbody {\n  font-family: Roboto, Noto Sans SC, Noto, Helvetica, Arial, sans-serif;\n}\n\n.mdui-container {\n  max-width: 950px;\n}\n\n.mdui-drawer-close {\n  display: none;\n}\n\n.mdui-drawer .mdui-list-item {\n  padding-left: 20px;\n  border-radius: 0 50px 50px 0;\n}\n\n.mdui-toolbar > a,\n.mdui-chip-title,\n.mdui-dialog-title {\n  font-family: Product Sans, Noto Sans SC, Roboto, Noto, Helvetica, Arial,\n    sans-serif;\n}\n\nfooter a {\n  text-decoration: none;\n}\n\n.mdui-chip {\n  margin: 20px;\n}\n\n.mdui-typo h1,\n.mdui-typo h2,\n.mdui-typo h3,\n.mdui-typo h4,\n.mdui-typo h5,\n.mdui-typo h6 {\n  font-family: Product Sans, Noto Sans SC;\n  margin-top: 0.2em;\n}\n\n@media (max-width: 740px) {\n  .mdui-col-sm-2,\n  .mdui-col-sm-3,\n  .forcedownload {\n    display: none;\n  }\n\n  .mdui-col-xs-12 {\n    width: 100%;\n  }\n\n}\n\n@media (max-width: 1024px) {\n  .mdui-drawer .mdui-list-item {\n    margin-right: 5px;\n  }\n}\n\n.mdui-list-item-icon {\n  color: black;\n}\n\n.mdui-typo {\n  border-radius: 8px;\n}\n\n.mdui-dialog {\n  border-radius: 8px;\n}\n\n.mdui-container .mdui-list-item {\n  border-radius: 50px;\n}\n\n#file {\n  margin-top: 1%;\n}\n\n#head-md,\n#readme-md {\n  display: none;\n}\n\n.mdui-theme-layout-dark .hljs-keyword, .hljs-selector-tag{\n  color:#dadada !important;\n}";
+
+  const styleEl = document.createElement("style");
+  const codeEl = document.createTextNode(code);
+  styleEl.type = 'text/css';
+  styleEl.appendChild(codeEl);
+  document.head.appendChild(styleEl);
+}
---
+++ renexmoe_assets/js/pack.js
@@ -0,0 +1,1 @@
+const e={version:"v1.1.3-lts",builder:"build-186526"};export default e;
